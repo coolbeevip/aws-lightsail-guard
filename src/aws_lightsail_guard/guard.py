d66475545d33b1e5d02b9b8267e9fb3abe6758a7
Lei Zhang <zhanglei@apache.org>
2023-11-06 18:36:24 +0800

    Config socket timeout 30 sec.

--- src/aws_lightsail_guard/guard.py
+++ src/aws_lightsail_guard/guard.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from aws_lightsail_guard.lightsail import lightsail
+from aws_lightsail_guard.lightsail import lightsail, lightsail_domain
 from aws_lightsail_guard.utils import check_address
 
 
@@ -15,19 +15,23 @@ class Guard:
             print(f"Instance {instance['name']} public static ip {instance['publicIpAddress']} is dead")
 
             # Allocate new static ip
+            new_static_ip = None
             new_static_ip_name = "IP-" + datetime.now().strftime("%Y%m%d%H%M%S")
             allocate_static_ip_response = lightsail.allocate_static_ip(
                 staticIpName=new_static_ip_name
             )
+            new_static_ip = allocate_static_ip_response['staticIp']['ipAddress']
             print(f"Allocate public ip {allocate_static_ip_response} success")
 
             # Release old static ip
+            released_old_static_ip = None
             for static_ip in lightsail.get_static_ips()['staticIps']:
                 if static_ip['ipAddress'] == instance['publicIpAddress']:
                     try:
                         release_static_ip_response = lightsail.release_static_ip(
                             staticIpName=static_ip['name'])
                         print(f"Release public ip {release_static_ip_response} success")
+                        released_old_static_ip = static_ip['ipAddress']
                         break
                     except Exception as e:
                         print(f"Release public ip {release_static_ip_response} fails")
@@ -38,6 +42,22 @@ class Guard:
                 instanceName=instance['name']
             )
             print(f"Attach public ip {attach_static_ip_response} success")
+
+            # Update domain entry to new static ip
+            get_domains_response = lightsail_domain.get_domains()
+            for domain in get_domains_response['domains']:
+                for domainEntry in domain['domainEntries']:
+                    if domainEntry['target'] == released_old_static_ip:
+                        lightsail_domain.update_domain_entry(
+                            domainName=domain['name'],
+                            domainEntry={
+                                'id': domainEntry['id'],
+                                'name': domainEntry['name'],
+                                'target': new_static_ip,
+                                'type': domainEntry['type'],
+                            }
+                        )
+                        print(f"Update domain entry {domainEntry['name']} to {new_static_ip} success")
 
         self.get_lightsail_instance_info(name)
 
@@ -51,6 +71,11 @@ class Guard:
         print(f"---------- static ips ----------")
         for static_ip in static_ips:
             print(f"{static_ip['name']}/{static_ip['ipAddress']}/{static_ip['attachedTo']}")
+        print(f"---------- domain ----------")
+        get_domains_response = lightsail_domain.get_domains()
+        for domain in get_domains_response['domains']:
+            for domainEntry in domain['domainEntries']:
+                print(f"{domain['name']}/{domainEntry['name']}/{domainEntry['target']}")
 
 
 guard = Guard()
--- src/aws_lightsail_guard/lightsail.py
+++ src/aws_lightsail_guard/lightsail.py
@@ -6,3 +6,8 @@ lightsail = boto3.client('lightsail',
                          region_name=os.environ['REGION_NAME'],
                          aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                          aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+
+lightsail_domain = boto3.client('lightsail',
+                                region_name='us-east-1',
+                                aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+                                aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
