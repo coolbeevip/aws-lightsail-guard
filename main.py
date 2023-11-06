4438ff3a88166bdde3a9a52b3a0d679944602ff0
Lei Zhang <zhanglei@apache.org>
2023-11-07 00:35:21 +0800

    Add CI

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
 import os
 import time
-from datetime import datetime
 
 import schedule
 
@@ -9,15 +7,11 @@ from aws_lightsail_guard.guard import guard
 
 
 def job_wrapper():
-    current_hour = datetime.now().hour
-    if 6 <= current_hour < 24:
-        guard.lightsail_instance_public_ip_keepalive(name=os.environ['LIGHTSAIL_INSTANCE_NAME'])
-    else:
-        logging.info('Skip')
+    guard.lightsail_instance_public_ip_keepalive(name=os.environ['LIGHTSAIL_INSTANCE_NAME'])
 
 
 if __name__ == '__main__':
-    schedule.every(1).minutes.do(job_wrapper)
+    schedule.every(5).minutes.do(job_wrapper)
 
     while True:
         schedule.run_pending()
