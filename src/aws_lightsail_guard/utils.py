0d08f47587439c3674d7f8125efb7d039fe76bd7
Lei Zhang <zhanglei@apache.org>
2023-11-06 12:04:57 +0800

    Config socket timeout 30 sec.

--- src/aws_lightsail_guard/utils.py
+++ src/aws_lightsail_guard/utils.py
@@ -4,6 +4,7 @@ import socket
 def check_address(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     location = (host, port)
+    sock.settimeout(30)
     result = sock.connect_ex(location)
     sock.close()
     return result == 0
